e8301b16f241699731c7876780e5b6bd606d972a
joonmoogo <junemuk10@gmail.com>
2024-06-03 17:16:17 +0900

    android

--- App.js
+++ App.js
@@ -1,48 +1,114 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import messaging from '@react-native-firebase/messaging'
 import WebView from 'react-native-webview';
 import { initializeApp } from 'firebase/app';
+import messaging from '@react-native-firebase/messaging';
 
+
+    /*    
+    
+    * TO INSTALL ANDROID *
+    1. command: npx expo run:android
+
+    * TO IMPLEMENTS WEBVIEW APP SUPPORTING PUSH NOTIFACTION *
+    1. get Native Device Token;
+    2. store Token at AsyncStorage;
+    option.1. send token using InjectedJavascripts 
+    = injectedJavaScript='sendToken(devicetoken)'      
+    option.2. send token using querystring
+    = https://www.choeaecafe.com?devicetoken={devicetoken}
+
+    should know:: Webview Compoennts can't issue token and doesn't support push api
+    so recommended to develop communication native clinet and webView
+
+    */
+
+    
 export default function App() {
 
-  messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-    //  remoteMessage.data로 메세지에 접근가능
-    //  remoteMessage.from 으로 topic name 또는 message identifier
-    //  remoteMessage.messageId 는 메시지 고유값 id
-    //  remoteMessage.notification 메시지와 함께 보내진 추가 데이터
-    //  remoteMessage.sentTime 보낸시간
-  });
 
+    async function requestUserPermission() {
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+    
+      if (enabled) {
+        console.log('Authorization status:', authStatus);
+      }
+    }
 
-  // Foreground 상태인 경우
-  useEffect(() => {
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-    });
-    return unsubscribe;
-  });
+    const getToken = async() =>{
+        // const token = await messaging().getToken();
+        // console.log("token = ",token)  
+        console.log('token')
+    }
+
+    useEffect(()=>{
+        requestUserPermission();
+        getToken();
+    },[])
 
   return (
     <WebView
-      source={{ uri: 'test.choeaecafe.com' }}
+      source={{ uri: 'choeaecafe.com' }}
       style={{ flex: 1, marginTop: 30 }}
     />
-    /* 
-      TO IMPLEMENTS WEBVIEW APP SUPPORTING PUSH NOTIFACTION
-
-      1. get Native Device Token;
-      2. store Token at AsyncStorage;
-      option.1. send token using InjectedJavascripts 
-      = injectedJavaScript='sendToken(devicetoken)'      
-      option.2. send token using querystring
-      = https://www.choeaecafe.com?devicetoken={devicetoken}
-
-
-      should know:: Webview Compoennts can't issue token and doesn't support push api
-      so recommended to develop communication native clinet and webView
-
-    */
   );
 }
+
+// import { StyleSheet, Text, View, TouchableOpacity} from "react-native";
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+// import { app } from "./firebaseConfig.js";
+
+// export default function App() {
+//     function signUp() {
+//         const auth = getAuth(app);
+
+//         createUserWithEmailAndPassword(
+//             auth,
+//             "junemuk10@gmail.com",
+//             "junmugo0220"
+//         )
+//             .then((res) => console.log(res))
+//             .catch((err) => console.log(err));
+            
+//     }
+
+//     return (
+//         <View style={styles.container}>
+//             <Text style={styles.text}>Check For Firebase Integration!</Text>
+
+//                 <TouchableOpacity style={styles.button_container} onPress={signUp}>
+//                 <Text style={styles.button_text}>SignUp</Text>
+//             </TouchableOpacity>
+//         </View>
+//     );
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         justifyContent: "center",
+//         marginTop: 48,
+//     },
+//     text: {
+//         fontWeight:"bold",
+//         textAlign:"center",
+//         fontSize:24,
+//     },
+//     button_text: {
+//         textAlign:"center",
+//         fontSize:24,
+//         color:"#1976d2"
+//     },
+//     button_container: {
+//         borderRadius: 15,
+//         flexDirection: "row",
+//         margin: 16,
+//         padding:24,
+//         justifyContent:"center",
+//         backgroundColor:"#e6e6e6"
+//     },
+// });
---
+++ firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCSrDJoUESxF5ditmidrCB9HwsYsClqBoI",
+  authDomain: "test-46500.firebaseapp.com",
+  projectId: "test-46500",
+  storageBucket: "test-46500.appspot.com",
+  messagingSenderId: "308631114520",
+  appId: "1:308631114520:web:a70ab56e49512c081d046d",
+  measurementId: "G-6RSNLGZ8ZE"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
